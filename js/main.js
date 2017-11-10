98e3d0257f557efc9eeff130c86b0cf01bef1352
Nina <nina4klemencic@gmail.com>
2017-11-10 14:03:19 +0100

    assignment 9

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.")
+	}
+}
 
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-
+var flipCard = function (cardId) {
+	checkForMatch();
+	console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
 if(cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
 	alert('You found a match!');
 } 
 else {
 	alert('Sorry, try again.');
 }
+
+
+}
+
+flipCard(0);
+flipCard(2);
